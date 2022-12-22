413a5fb3ba1faf715e84cd568bba65359b180cd1
niluferdone <77413323+niluferdone@users.noreply.github.com>
2022-12-22 15:38:29 +0300

    Add files via upload

---
+++ constants.py
@@ -0,0 +1,24 @@
+# Constant olması için büyük harflerle yazılır
+BASE_DOMAIN_URL = "https://www.saucedemo.com/" # domain url 
+USERNAME_INFO = "user-name" # id bilgisi
+USERNAME = "standard_user"
+PASSWORD_INFO = "password" # id bilgisi
+PASSWORD = "secret_sauce"
+LGN_BUTTON = "login-button" # id bilgisi
+LOGIN = "https://www.saucedemo.com/inventory.html" # login olduktan sonraki açılan sayfa
+
+ERROR = '//*[@data-test="error"]' # hata ekranı başlığı (xpath)
+ERROR_TEXT = "Epic sadface: Username and password do not match any user in this service" # hata mesajı 
+WRONG_PASSWORD = "wrong_password"
+
+MENU = "react-burger-menu-btn" # menü butonu 
+MENU_TEXT = "Open Menu" 
+
+PRODUCT_LIST = '//*[@class="inventory_item"]'  # ürünü açıklayan kısım (xpath)
+
+ADD_TO_CARD = "add-to-cart-sauce-labs-backpack" # add to card butonu için (id bilgisi)
+PRODUCT_REMOVE = "remove-sauce-labs-backpack" # remove butonu için (name bilgisi)
+REMOVE_TEXT = "REMOVE" # remove butonu için 
+
+BASKET_ITEM = "shopping_cart_badge" # item butonu için (class name)
+ITEM_COUNT = "1" 
