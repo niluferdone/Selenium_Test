8685523c09b26119eae8590ffdf06c8c5607ccea
niluferdone <77413323+niluferdone@users.noreply.github.com>
2022-12-22 15:55:44 +0300

    Update test_workshop.py

--- test_workshop.py
+++ test_workshop.py
@@ -1,5 +1,4 @@
 from selenium import webdriver
-from time import sleep
 from selenium.webdriver.support.wait import WebDriverWait
 from selenium.webdriver.support import expected_conditions
 from selenium.webdriver.common.by import By
